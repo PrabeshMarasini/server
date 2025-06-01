8414b29a59dfbd333848de968bd98f50e01a05eb
Prabesh Marasini <prabeshmarasini12345@gmail.com>
2025-06-01 14:35:25 +0545

    Client file

---
+++ include/client.hpp
@@ -0,0 +1,6 @@
+#ifndef CLIENT_HANDLER_HPP
+#define CLIENT_HANDLER_HPP
+
+void handle_client(int client_fd);
+
+#endif
---
+++ src/client.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <unistd.h>     
+#include <cstring>
+#include "client.hpp"    
+
+constexpr int BUFFER_SIZE = 1024;
+
+void handle_client(int client_fd) {
+    char buffer[BUFFER_SIZE];
+    memset(buffer, 0, sizeof(buffer));
+
+    const char* welcome = "Welcome to MNA Remote Server.\n";
+    send(client_fd, welcome, strlen(welcome), 0);
+
+    ssize_t bytes_received = recv(client_fd, buffer, BUFFER_SIZE - 1, 0);
+    if (bytes_received > 0) {
+        buffer[bytes_received] = '\0';
+        std::cout << "[Client] " << buffer << "\n";
+
+        const char* reply = "ACK: Message received.\n";
+        send(client_fd, reply, strlen(reply), 0);
+    } else {
+        std::cout << "[Client] No data received or connection closed.\n";
+    }
+}
