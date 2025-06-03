b8f43359890b37f797d1964b21394d52915f8fbd
Prabesh Marasini <prabeshmarasini12345@gmail.com>
2025-06-03 18:19:49 +0545

    Update client main to use new packet reception interface

--- client/main.cpp
+++ client/main.cpp
@@ -1,31 +1,36 @@
 #include <iostream>
+#include <csignal>
+#include <atomic>
 #include "tls_client.hpp"
 
-int main() {
-    TLSClient client("127.0.0.1", 5555);
+std::atomic<bool> running(true);
 
+void signal_handler(int signum) {
+    std::cout << "\nSignal " << signum << " received, shutting down client...\n";
+    running = false;
+}
+
+int main() {
+    std::signal(SIGINT, signal_handler);
+    std::signal(SIGTERM, signal_handler);
+    
+    TLSClient client("127.0.0.1", 5555);
+    
     if (!client.init()) {
         std::cerr << "Failed to initialize TLS client\n";
         return 1;
     }
-
+    
     if (!client.connect_server()) {
         std::cerr << "Failed to connect to server\n";
         return 1;
     }
-
-    if (!client.send_data("Hello from TLS client!\n")) {
-        std::cerr << "Failed to send data\n";
-    }
-
-    std::string response = client.receive_data();
-    if (!response.empty()) {
-        std::cout << "Received from server: " << response << "\n";
-    } else {
-        std::cout << "No response or error receiving data\n";
-    }
-
+    
+    std::cout << "Successfully connected to server. Receiving packet data...\n";
+    client.receive_and_process_packets();
+    
+    std::cout << "Cleaning up and shutting down...\n";
     client.cleanup();
-
+    
     return 0;
 }
